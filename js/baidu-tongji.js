d71f1c79c4fb7c77a519c92050ac1b2c274ce806
ChenDark <53772533+YouChenJun@users.noreply.github.com>
2025-03-10 15:04:53 +0800

    Deployed 7ab8f03 with MkDocs version: 1.6.1

---
+++ js/baidu-tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b7be72b509eae73cb8ca0674117a4bda";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
